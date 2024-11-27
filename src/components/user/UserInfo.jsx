cad1b4ac183922c761e1ee23e48314d87aec488e
yeong1110 <a0257942@gmail.com>
2024-11-27 23:30:41 +0900

    style : 보카박스 기능소개 작업(스와이퍼 적용), 완료페이지 애니메이션 작업

--- src/components/user/UserInfo.jsx
+++ src/components/user/UserInfo.jsx
@@ -3,6 +3,7 @@ import {useState, useRef, useEffect} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/pagination';
 
 function UserInfo(){
 	const compAnimation = useRef();
@@ -20,28 +21,34 @@ function UserInfo(){
 				이런 기능이 있어요
 				</h2>
 				<div className="comp_ani_box">
-				<div className="comp_swiper_wrap">
+					<div className="comp_swiper_wrap">
 					<Swiper
 						loop={true}
 						pagination={true}
 						modules={[Pagination]}
 						className="comp_swiper"
 					>
-						<SwiperSlide className="comp_swiper_slide">Slide 1</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 2</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 3</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 4</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 5</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 6</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 7</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 8</SwiperSlide>
-						<SwiperSlide className="comp_swiper_slide">Slide 9</SwiperSlide>
+						<SwiperSlide className="comp_swiper_slide">
+							<p className="comp_swiper_title">나만의 단어장을 만들어요</p>
+						</SwiperSlide>
+						<SwiperSlide className="comp_swiper_slide">
+							<p className="comp_swiper_title">단어를 추가하고 수정해요</p>
+						</SwiperSlide>
+						<SwiperSlide className="comp_swiper_slide">
+							<p className="comp_swiper_title">내 마음대로 커스텀해요</p>
+						</SwiperSlide>
+						<SwiperSlide className="comp_swiper_slide">
+							<p className="comp_swiper_title">오늘은 무엇을 배웠나요?</p>
+						</SwiperSlide>
+						<SwiperSlide className="comp_swiper_slide">
+							<p className="comp_swiper_title">내가 추가한 단어를 학습해요</p>
+						</SwiperSlide>
 					</Swiper>
 					</div>
 				</div>
 			</div>
 			<div className="comp_btn_wrap comp_ani_btn">
-				<button className="btn-fill sizeL" >다음</button>
+				<button className="btn-fill sizeL" >사용해보기</button>
 			</div>
 		</div>
 	);
