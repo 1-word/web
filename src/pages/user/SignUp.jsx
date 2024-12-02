18d3e3b4764ca5354d1fd8567dee3d3b7b0e8492
yeong1110 <a0257942@gmail.com>
2024-12-02 21:12:58 +0900

    style: -signup : 스타일 변경

--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -2,10 +2,14 @@ import LOGOTitle_SVG from "@images/logoTitle.svg";
 import React, { useState, useRef } from "react";
 import api, { MODE } from "@/services/api";
 import { Link } from "react-router-dom";
+import AuthNum from "@/components/user/AuthNum";
 
 function Signup(){
+	const patternEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]/;
 	const signupData = useRef(null);
 	const [mailState,setMailState] = useState(false);
+	const [disabled , setDisabled] = useState(true);
+	const [disabledAll, setDisabledAll] = useState(true);
 	const onClickHandler = api();
 
 	const handleClick = (e) => {
@@ -43,6 +47,7 @@ function Signup(){
 	const setSignupInput = e => {
 			const data = signupData.current;
 			const { value, name } = e.target;
+			const mailReg = patternEmail.test(value);
 
 			setSignupData({
 					...data,
@@ -54,6 +59,13 @@ function Signup(){
 					!checkPassword(value, data?.password)) {
 					// 아래 메시지를 출력해줄 text 추가 필요
 					console.log("비밀번호와 일치하지 않습니다.");
+			}
+			if(value === undefined || value === "" || value === null){
+				setDisabled(true);
+			}else{
+				if(mailReg){
+					setDisabled(false);
+				}
 			}
 	}
 
@@ -67,46 +79,39 @@ function Signup(){
 				<h1 className="login-title"><Link to="/">VOCABOX<img src={LOGOTitle_SVG} alt="VOCABOX" /></Link></h1>
 					<div className="login-cont">
 							<div className="login-area">
-									<legend className="disN">회원가입</legend>
+									<h2 className="disN">회원가입</h2>
 									{/* 에러 메시지 :: error : 에러 / correct : 통과 */}
 									<div className="login-input-area input_wrap">
 											<label htmlFor="email">이메일</label>
 											<div className="auth_num_area">
-												<input id="email" name="email" type="email" spellCheck placeholder="이메일" onChange={setSignupInput} onKeyDown={onKeyDown} />
-												<button className="btn-fill auth_num_btn sizeM" onClick={handleCheckMailAuth}>이메일 인증</button>
+												<input id="email" name="email" type="email" spellCheck placeholder="이메일주소를 입력해주세요" onChange={setSignupInput} onKeyDown={onKeyDown} />
+												<button className="btn-fill auth_num_btn sizeM" disabled={disabled} onClick={handleCheckMailAuth}>메일 인증</button>
 											</div>
-											<p className="msg error">에러입니다</p>
+											<p className="msg error">이미 사용중인 메일 주소입니다.</p>
 									</div>
-									<div className="login-input-area input_wrap auth_num_wrap">
-										<label htmlFor="auth_num"></label>
-										<div className="auth_num_area">
-											<input id="auth_num" name="" type="text" spellCheck placeholder="인증번호"  />
-											<button className="btn-fill auth_num_btn sizeM">재전송</button>
-											<div className="auth_num_count">05 : 00</div>
-										</div>
-										<p className="msg error">입력한 인증번호가 맞지 않습니다</p>
-									</div>
-									<div className="login-btn-wrap">
-										<button className="btn-fill btn-login sizeL">인증번호 확인</button>
-									</div>
-									<div className="login-input-area input_wrap">
+									<>
+										{
+											mailState ? <AuthNum></AuthNum> : ""
+										}
+									</>
+									{/* <div className="login-input-area input_wrap">
 											<label htmlFor="nickname">이름</label>
 											<input id="nickname" name="nickname" type="text" spellCheck placeholder="이름" onChange={setSignupInput} onKeyDown={onKeyDown} />
 											<p className="msg error">에러입니다</p>
-									</div>
+									</div> */}
 									<div className="login-input-area input_wrap">
 											<label htmlFor="password">비밀번호</label>
 											<input id="password" type="password" name="password" spellCheck placeholder="비밀번호" onChange={setSignupInput} onKeyDown={onKeyDown}/>
-											<p className="msg error">에러입니다</p>
+											<p className="msg error">8~20자, 영문+숫자/특수문자를 포함해야 합니다.</p>
 									</div>
 									<div className="login-input-area input_wrap">
 											<label htmlFor="confirm_password">비밀번호 확인</label>
 											<input id="confirm_password" type="password" name="confirm_password" spellCheck placeholder="비밀번호 확인" onChange={setSignupInput} onKeyDown={onKeyDown}/>
-											<p className="msg error">에러입니다</p>
+											<p className="msg error">비밀번호가 맞지 않습니다</p>
 											<p className="msg correct">통과입니다</p>
 									</div>
 									<div className="login-btn-wrap">
-									<button className="btn-fill btn-login sizeL" onClick={handleClick}>회원가입</button>
+									<button className="btn-fill btn-login sizeL" disabled={disabledAll} onClick={handleClick}>회원가입</button>
 									</div>
 									<div className="login-join-wrap">
 											<p>이미 회원가입을 하셨나요? <Link to='/signin'>로그인</Link></p>
