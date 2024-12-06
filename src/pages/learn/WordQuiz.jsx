a0cdea2410fd717ead5b57011de4afff84a7a745
yeong1110 <a0257942@gmail.com>
2024-12-06 13:08:20 +0900

    style : -quiz : 퀴즈, 퀴즈 결과 컴포넌트 추가

--- src/pages/learn/WordQuiz.jsx
+++ src/pages/learn/WordQuiz.jsx
@@ -3,8 +3,20 @@ import HeaderMini from "@/components/layout/header_mini";
 import LeftFix from "@components/layout/left_fix";
 import BottomNav from "@components/layout/bottom_nav";
 import VocabookList from "@/components/word/folder/VocaBookList";
+import Quiz from "@/components/learn/quiz/Quiz";
+import Result from "@/components/learn/quiz/Result";
 
 function WordQuiz(){
+	const [next,nextState] = useState(
+		{
+			quiz : false,
+			result : false
+		}
+	);
+
+	const handleQuizPhaze = () => {
+		nextState({quiz:true});
+	}
 	return(
 		<div className="wrap">
 			<HeaderMini title="단어 학습"></HeaderMini>
@@ -12,15 +24,28 @@ function WordQuiz(){
 			<BottomNav active="learn"></BottomNav>
 			<div className="word_quiz_wrap">
 				<div className="word_quiz_cont">
-					<h2 className="word_quiz_title">단어 학습에 사용할 단어장을 선택해주세요</h2>
-					<ul className="word_quiz_vocabook_lists">
-						<li>
-							단어장1
-						</li>
-					</ul>
-					<div className="word_quiz_btn_wrap">
-						<button className="btn-fill sizeM">다음으로</button>
-					</div>
+					{
+						(next.quiz) === true ? "" :
+						<>
+							<h2 className="word_quiz_title">단어 학습에 사용할 단어장을 선택해주세요</h2>
+							<ul className="word_quiz_vocabook_lists">
+								<li>
+									단어장1
+								</li>
+								<li>
+									단어장1
+								</li>
+							</ul>
+							<div className="word_quiz_btn_wrap">
+								<button className="btn-fill sizeM" onClick={handleQuizPhaze}>다음 단계</button>
+							</div>
+						</>
+					}
+					{
+						(next.quiz) === true ? <Quiz></Quiz> :
+						(next.result) === true ? <Result></Result> :
+						""
+					}
 				</div>
 			</div>
 		</div>
