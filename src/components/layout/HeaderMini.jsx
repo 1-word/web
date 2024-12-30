d293367ec0f788b855ad611f2a1a8c81a2fb1fff
yeong1110 <a0257942@gmail.com>
2024-12-30 13:02:10 +0900

    style: -header fixed 추가 -게시판 리스트 토글 추가, 애니메이션 추가

--- src/components/layout/HeaderMini.jsx
+++ src/components/layout/HeaderMini.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const HeaderMini = ({idx, handleClick, compCloseAction, title}) => {
+const HeaderMini = ({idx, handleClick, compCloseAction, title, fixed}) => {
 	const navigate = useNavigate();
 return(
-	<header className="mini flex">
+	<header className={fixed? "mini flex fixed" : "mini flex"}>
 		{
 			(title==="단어장") ?
 			<button></button>
