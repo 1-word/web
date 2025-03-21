44402f09b71e950866fe535cacc51119d953206c
hyun <apfhd0257@naver.com>
2025-03-21 18:42:01 +0900

    style: lounge list component, scss

---
+++ src/components/lounge/EveryList.jsx
@@ -0,0 +1,13 @@
+const EveryList = () => {
+  return(
+    <li>
+      <div>
+      <p className="lounge-list-title">title</p>
+      <p className="lounge-list-title-sub">00 님의 단어장</p>
+      <p className="lounge-list-title-sub">총 단어 : 00개</p>
+      </div>
+    </li>
+  )
+}
+
+export default EveryList;
---
+++ src/components/lounge/UserList.jsx
@@ -0,0 +1,19 @@
+const UserList = () => {
+  return(
+    <li>
+      <div>
+      <p className="lounge-list-title">title</p>
+      <button className="lounge-list-toggle toggle">
+      <i className="xi-angle-down"></i>
+      </button>
+      </div>
+      <div>
+        <div>
+        토글될애
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default UserList;
