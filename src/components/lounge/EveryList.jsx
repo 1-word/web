64bdba098f25a9e0df6ce0739f70324c8c9dab5d
yeong1110 <a0257942@gmail.com>
2025-03-24 20:04:28 +0900

    style: 라운지 나의 단어장 퍼블리싱 작업

--- src/components/lounge/EveryList.jsx
+++ src/components/lounge/EveryList.jsx
@@ -1,13 +1,22 @@
 const EveryList = () => {
-  return(
+  return (
     <li>
       <div>
-      <p className="lounge-list-title">title</p>
-      <p className="lounge-list-title-sub">00 님의 단어장</p>
-      <p className="lounge-list-title-sub">총 단어 : 00개</p>
+        <div className="lounge-list-title">
+          {/* 단어장 색깔 넣어주기 */}
+          <div
+            className="lounge-list-title-dot"
+            style={{
+              background: "#000",
+            }}
+          ></div>
+          <p>title</p>
+        </div>
+        <p className="lounge-list-title-sub">00 님의 단어장</p>
+        <p className="lounge-list-title-sub">총 단어 : 00개</p>
       </div>
     </li>
-  )
-}
+  );
+};
 
 export default EveryList;
--- src/components/lounge/UserList.jsx
+++ src/components/lounge/UserList.jsx
@@ -1,19 +1,56 @@
+import { useState } from "react";
+import { useModal } from "@/hook/_hooks";
+import FullModal from "@components/layout/popup/FullModal";
+import WordBookPermission from "@components/lounge/WordBookPermission";
+
 const UserList = () => {
-  return(
+  const [isToggle, setToggle] = useState(false);
+  const [permissionModal] = useModal("wordbookPermission");
+  const handlelistToggle = () => {
+    setToggle(!isToggle);
+  };
+  const handleModal = () => (e) => {
+    permissionModal(FullModal, WordBookPermission);
+  };
+  return (
     <li>
-      <div>
-      <p className="lounge-list-title">title</p>
-      <button className="lounge-list-toggle toggle">
-      <i className="xi-angle-down"></i>
-      </button>
-      </div>
-      <div>
-        <div>
-        토글될애
+      <div onClick={handlelistToggle}>
+        <div className="lounge-list-title">
+          {/* 단어장 색깔 넣어주기 */}
+          <div
+            className="lounge-list-title-dot"
+            style={{
+              background: "#000",
+            }}
+          ></div>
+          <p>title</p>
         </div>
+        <button className="lounge-list-toggle toggle">
+          <i className={isToggle ? "xi-angle-down" : "xi-angle-up"}></i>
+        </button>
       </div>
+      {isToggle && (
+        <div>
+          <div className="lounge-list-toggle-wrap">
+            <div>
+              쉐어룸 공개
+              <div className="switch-wrap">
+                <label className="switch">
+                  {/* checked로 체킹 가능 :  checked={false}*/}
+                  <input type="checkbox" />
+                  <span className="slider" />
+                </label>
+              </div>
+            </div>
+            <div onClick={handleModal()}>
+              권한 설정
+              <i className="xi-angle-right"></i>
+            </div>
+          </div>
+        </div>
+      )}
     </li>
-  )
-}
+  );
+};
 
 export default UserList;
