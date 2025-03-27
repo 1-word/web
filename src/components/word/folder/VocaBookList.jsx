fcab69eb0ca6e204dd7140c4a834c893896c2240
yeong1110 <a0257942@gmail.com>
2025-03-28 00:24:33 +0900

    style: 단어장 리스트 페이지 디자인변경 및 레이아웃 컴포넌트 변경

--- src/components/word/folder/VocaBookList.jsx
+++ src/components/word/folder/VocaBookList.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import api, {MODE} from "@/services/api";
+import React, { useEffect, useState } from "react";
+import api, { MODE } from "@/services/api";
 import wordListStore from "@/store/wordListStore";
 import { useModal } from "@/hook/_hooks";
 import AddVocaBook from "@/components/word/folder/AddVocaBook";
@@ -12,50 +12,49 @@ function VocabookList({
   clickedFolder,
   afterCompleteFunc,
   deleteModalAfterTime,
-  props
-}){
+  props,
+}) {
   const [editState, setEditState] = useState(false);
-  const [folderList, setFolderList] = useState([{id: -1}]);
+  const [folderList, setFolderList] = useState([{ id: -1 }]);
   const [addFolderModal] = useModal("addFolder");
-  const { updateStart } = wordListStore(state => state);
-  const {setStoreFolderList} = authStore(state => state);
+  const { updateStart } = wordListStore((state) => state);
+  const { setStoreFolderList } = authStore((state) => state);
   const navigate = useNavigate();
 
   const onClickHandler = api();
 
   useEffect(() => {
-    onClickHandler(null, MODE.FOLDER_READ)
-    .then(res => {
+    onClickHandler(null, MODE.FOLDER_READ).then((res) => {
       setFolderList(res);
-    })
-  }, []); 
-
-  const handleAddClick = () => e => {
-    addFolderModal(FullModal, AddVocaBook, {
-      setFolderList
     });
-  }
+  }, []);
 
-  const handleConfigClick = () => {
-	  setEditState(!editState);
-  }
-  
-  const onEditClick = (folder) => e => {
+  const handleAddClick = () => (e) => {
     addFolderModal(FullModal, AddVocaBook, {
       setFolderList,
-      prevFolder: folder
-    })
-  }
-
-  const onDeleteClick = (id) => e => {
-    onClickHandler(null, MODE.FOLDER_DELETE, id).then(_ => {
-      setFolderList(prev => {
-        return prev.filter(folder => folder.wordBookId !== id)
-      })
     });
-  }
+  };
 
-  const onFolderClick = (item) => e => {
+  const handleConfigClick = () => {
+    setEditState(!editState);
+  };
+
+  const onEditClick = (folder) => (e) => {
+    addFolderModal(FullModal, AddVocaBook, {
+      setFolderList,
+      prevFolder: folder,
+    });
+  };
+
+  const onDeleteClick = (id) => (e) => {
+    onClickHandler(null, MODE.FOLDER_DELETE, id).then((_) => {
+      setFolderList((prev) => {
+        return prev.filter((folder) => folder.wordBookId !== id);
+      });
+    });
+  };
+
+  const onFolderClick = (item) => (e) => {
     const id = item.wordBookId;
     e.preventDefault();
 
@@ -67,8 +66,8 @@ function VocabookList({
     if (!afterCompleteFunc) {
       navigate(`/word/${id}`);
       updateStart();
-      setStoreFolderList(item)
-      if(deleteModalAfterTime) {
+      setStoreFolderList(item);
+      if (deleteModalAfterTime) {
         deleteModalAfterTime(0);
       }
       return;
@@ -77,49 +76,67 @@ function VocabookList({
     // 클릭된 폴더 데이터 넘겨주기
     afterCompleteFunc(item, props);
     deleteModalAfterTime(0);
-  }
+  };
 
   return (
     <div className="voca_book_wrap">
-	  <div className="voca_book_cont">
-		<div className="voca_book_top flex">
-          <button onClick={handleConfigClick}>단어장 관리</button>
-			<button className="voca_book_plus" onClick={handleAddClick()}>새 단어장 만들기<i className="xi-plus"></i></button>
-		</div>
-				{
-					// 단어장 없을 경우 체크
-					folderList.length > 0? '' : 
-					<ListEmpty title={"단어장이"} content={"+ 버튼을 눌러 새 단어장을 추가해주세요"} />
-				}
+      <div className="voca_book_cont">
+        <div className="voca_book_top flex">
+          <button
+            className={editState ? "btn-fill sizeS" : "btn-light sizeS"}
+            onClick={handleConfigClick}
+          >
+						<i className="xi-cog"></i>
+            단어장 관리
+          </button>
+          <button className="btn-fill sizeS" onClick={handleAddClick()}>
+            새 단어장 만들기<i className="xi-plus"></i>
+          </button>
+        </div>
+        {
+          // 단어장 없을 경우 체크
+          folderList.length > 0 ? (
+            ""
+          ) : (
+            <ListEmpty
+              title={"단어장이"}
+              content={"+ 버튼을 눌러 새 단어장을 추가해주세요"}
+            />
+          )
+        }
         <ul className="voca_book_lists flex">
-          {
-            folderList?.map(item =>
-                // 현재 단어장 위치
-            <li className={clickedFolder === item?.wordBookId + ''? "on" : "off"} 
-                key={`folders${item?.wordBookId}`}
-                disabled={editState}
-                onClick={onFolderClick(item)}
+          {folderList?.map((item) => (
+            // 현재 단어장 위치
+            <li
+              className={clickedFolder === item?.wordBookId + "" ? "on" : "off"}
+              key={`folders${item?.wordBookId}`}
+              disabled={editState}
+              onClick={onFolderClick(item)}
             >
-            <div className="voca_book_list_area">
-							<div className="voca_book_color"
-									style={{ backgroundColor: item?.background || '#946CF4'}}>
-							</div>
-            <p className="voca_book_list_name">{item?.name}</p>
-              { editState &&
-                <div className="voca_book_list_btn_area">
+              <div className="voca_book_list_area">
+                <div
+                  className="voca_book_color"
+                  style={{ backgroundColor: item?.background || "#946CF4" }}
+                ></div>
+                <p className="voca_book_list_name">{item?.name}</p>
+                {editState && (
+                  <div className="voca_book_list_btn_area">
                     {/* 수정 */}
-                  <button onClick={onEditClick(item)}><i className="edit"></i></button>
+                    <button onClick={onEditClick(item)}>
+                      <i className="edit"></i>
+                    </button>
                     {/* 삭제 */}
-                  <button onClick={onDeleteClick(item?.wordBookId)}><i className="xi-close"></i></button>
-                </div>       
-              }
-                </div>
-                <div className="voca_book_list_wordamount">
-                    총 단어 갯수 : {item?.totalCount}개
-                </div>
-          </li>
-            )
-          }
+                    <button onClick={onDeleteClick(item?.wordBookId)}>
+                      <i className="xi-close"></i>
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="voca_book_list_wordamount">
+                총 단어 갯수 : {item?.totalCount}개
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
