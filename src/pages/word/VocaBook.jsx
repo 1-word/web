923ffe9aa6eafee125b3aeaad7b6d7c6e19b66b6
yeong1110 <a0257942@gmail.com>
2025-03-28 00:24:33 +0900

    style: 단어장 리스트 페이지 디자인변경 및 레이아웃 컴포넌트 변경

--- src/pages/word/VocaBook.jsx
+++ src/pages/word/VocaBook.jsx
@@ -1,39 +1,39 @@
-import HeaderMini from "@/components/layout/HeaderMini";
-import Footer from "@/components/layout/Footer";
-import BottomNav from "@/components/layout/BottomNav";
-import LeftFix from "@/components/layout/LeftFix";
 import VocabookList from "@/components/word/folder/VocaBookList";
 import { useEffect, useState } from "react";
+import Layout from "@/components/layout/Layout";
 
-function VocaBook(){
-	const [tutorialState,setTutorialState] = useState(false);
-	const closeTutorial = () => {
-		setTutorialState(true);
-	}
-// 튜토리얼 임의삭제 코드
-	useEffect(()=>{
-		setTutorialState(true);
-	},[])
-	return(
-		<div className="wrap">
-				<HeaderMini title="단어장"></HeaderMini>
-				<BottomNav active="word"></BottomNav>
-				<LeftFix></LeftFix>
-				<VocabookList></VocabookList>
-				{
-					!tutorialState ?
-					<div className="tutorial">
-					<div className="tutorial_fixed"></div>
-					<button className="tutorial_close" onClick={closeTutorial}>
-						<i className="xi-close-thin"></i>
-					</button>
-					<div className="tutorial_title tutorial_1">단어장을 수정 및 삭제할 수 있어요</div>
-					<div className="tutorial_title tutorial_2">새 단어장을 만들 수 있는 버튼이에요</div>
-					<div className="tutorial_title tutorial_3">새 단어를 추가하는 버튼이에요</div>
-				</div>
-				: ""
-				}
-		</div>
-	);
-};
+function VocaBook() {
+  const [tutorialState, setTutorialState] = useState(false);
+  const closeTutorial = () => {
+    setTutorialState(true);
+  };
+  // 튜토리얼 임의삭제 코드
+  useEffect(() => {
+    setTutorialState(true);
+  }, []);
+  return (
+    <Layout title="단어장" active="word">
+      <VocabookList></VocabookList>
+      {!tutorialState ? (
+        <div className="tutorial">
+          <div className="tutorial_fixed"></div>
+          <button className="tutorial_close" onClick={closeTutorial}>
+            <i className="xi-close-thin"></i>
+          </button>
+          <div className="tutorial_title tutorial_1">
+            단어장을 수정 및 삭제할 수 있어요
+          </div>
+          <div className="tutorial_title tutorial_2">
+            새 단어장을 만들 수 있는 버튼이에요
+          </div>
+          <div className="tutorial_title tutorial_3">
+            새 단어를 추가하는 버튼이에요
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </Layout>
+  );
+}
 export default VocaBook;
