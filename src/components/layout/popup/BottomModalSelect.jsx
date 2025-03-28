071f424c848941e873c73c4d078493adb7fc5dce
yeong1110 <a0257942@gmail.com>
2025-03-28 17:37:31 +0900

    style: 마이페이지 레이아웃 교체 및 디자인 수정 fix: 바텀모달 기능수행시 닫히도록 수정

--- src/components/layout/popup/BottomModalSelect.jsx
+++ src/components/layout/popup/BottomModalSelect.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useRef } from "react";
 
-function BottomModalSelect({setting,deleteModalAfterTime}){
-	return (
-		<div className="modal_bottom_select_wrap">
-			<ul className="modal_bottom_select_lists">
-				{
-				setting.map((data, idx)=>{
-					return <li key={idx} className="modal_bottom_select_list" onClick={() => data.onClick()}>{data.title}</li>;
-				})
-				}
-			</ul>
-			<div className="modal_bottom_select_btn_wrap">
-				<button onClick={() => deleteModalAfterTime(240)}>취소</button>
-			</div>
-		</div>
-	)
+function BottomModalSelect({ setting, deleteModalAfterTime }) {
+  return (
+    <div className="modal_bottom_select_wrap">
+      <ul className="modal_bottom_select_lists">
+        {setting.map((data, idx) => {
+          return (
+            <li
+              key={idx}
+              className="modal_bottom_select_list"
+              onClick={() => {
+                data.onClick(), deleteModalAfterTime(240);
+              }}
+            >
+              {data.title}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="modal_bottom_select_btn_wrap">
+        <button onClick={() => deleteModalAfterTime(240)}>취소</button>
+      </div>
+    </div>
+  );
 }
 
 export default BottomModalSelect;
--- src/pages/user/MyPage.jsx
+++ src/pages/user/MyPage.jsx
@@ -1,82 +1,57 @@
-import HeaderMini from "@/components/layout/HeaderMini";
-import BottomNav from "@/components/layout/BottomNav";
-import LeftFix from "@/components/layout/LeftFix";
-import MyDeault_SVG from "@images/myImgDefault.svg";
+import Layout from "@/components/layout/Layout";
+
 import api, { MODE } from "@/services/api";
 import { useModal } from "@/hook/_hooks";
 
-import BottomModalSelect from "@components/layout/popup/BottomModalSelect";
-import CenterModal from "@components/layout/popup/CenterModal";
-import CenterModalConfirm from "@/components/layout/popup/CenterModalConfirm";
 import FullModal from "@components/layout/popup/FullModal";
-import BottomModal from "@components/layout/popup/BottomModal";
 
 import UserConfig from "@components/user/UserConfig";
-import FolderList from "@/components/word/folder/VocaBookList";
 import ChangePw from "@/components/user/ChangePw";
 import Store from "@/store/store";
 import authStore from "@/store/authStore";
 import { Link } from "react-router-dom";
-import AddTypeMore from "@/components/modal/add/AddTypeMore";
 
-function MyPage(){
-	const [userConfigModal] = useModal("userConfig");
-	const [addTypeMoreModal] = useModal("addTypeMore");
-	const [deleteAccountModal] = useModal("deleteAccount");
-	const [changePwModal] = useModal("changePw");
-	const {userInfo} = authStore(state => state);
+function MyPage() {
+  const [userConfigModal] = useModal("userConfig");
 
-	const onClickHandler = api();
-	const handleUserConfig = () => e => {
-		userConfigModal(FullModal,UserConfig)
-	}
-	const handleAddTypeMore = () => e => {
-		addTypeMoreModal(FullModal,AddTypeMore)
-	}
-	const handleChangePwModal = () => e => {
-		changePwModal(FullModal,ChangePw)
-	}
+  const [changePwModal] = useModal("changePw");
+  const { userInfo } = authStore((state) => state);
 
-	const signout = (e) => {
-		onClickHandler(null, MODE.SIGNOUT);
-	}
+  const onClickHandler = api();
+  const handleUserConfig = () => (e) => {
+    userConfigModal(FullModal, UserConfig);
+  };
 
-	const handleDeleteAccountModal = () => e => {
-		deleteAccountModal(CenterModal,CenterModalConfirm,{
-			title: "정말 떠나시는 건가요?",
-			content: "계정 삭제시 이메일, 닉네임, 계정사진 및 모든 저장 사항이 삭제되어요. 그래도 진행하실 건가요?",
-			onClick: () => {
-				onClickHandler(null, MODE.USER_DELETE);
-			},
-		})
-	}
+  const handleChangePwModal = () => (e) => {
+    changePwModal(FullModal, ChangePw);
+  };
 
-	return (
-		<div className="wrap">
-			<LeftFix></LeftFix>
-			<BottomNav active="mypage"></BottomNav>
-			<HeaderMini title="마이페이지"></HeaderMini>
-			<div className="my_page_wrap">
-				<div className="my_user">
-					<div className="my_user_img">
-					<img src={userInfo?.profileImagePath ?? '/myImgDefault.svg'} alt="default" />
-					</div>
-					<span className="my_user_name">{userInfo?.nickname || ''}</span>님, 오늘도 힘내봐요!
-				</div>
-				<ul className="my_page_list">
-					<li onClick={handleUserConfig()}>내 계정 설정
-						<p className="my_page_email">{userInfo?.email}</p>
-					</li>
-					<li onClick={handleChangePwModal()}>비밀번호 변경</li>
-					<li onClick={handleAddTypeMore()}>내 품사 관리</li>
-					{/* 미구현 임시 none처리 */}
-					<li className="disN"><Link to="/notice">공지사항</Link></li>
-					<li onClick={signout}>로그아웃</li>
-					<li onClick={handleDeleteAccountModal()}>계정 삭제</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <Layout title="마이페이지" active="mypage">
+      <div className="my_page_wrap">
+        <div className="my_user">
+          <div className="my_user_img">
+            <img
+              src={userInfo?.profileImagePath ?? "/myImgDefault.svg"}
+              alt="default"
+            />
+          </div>
+          <span className="my_user_name">{userInfo?.nickname || ""}</span>님,
+          오늘도 힘내봐요!
+        </div>
+        <ul className="my_page_list">
+          <li onClick={handleUserConfig()}>
+            내 계정 설정
+            <p className="my_page_email">{userInfo?.email}</p>
+          </li>
+          <li onClick={handleChangePwModal()}>비밀번호 변경</li>
+          <li>
+            <Link to="/notice">공지사항</Link>
+          </li>
+        </ul>
+      </div>
+    </Layout>
+  );
 }
 
 export default MyPage;
